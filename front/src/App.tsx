9bcb13355c9c3262b5a097b42305c50d7c7f7be2
agherasie <alexandru.gherasie@epitech.eu>
2024-07-16 22:14:44 +0200

    fix: JSON RPC Provider

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import { useProviderStore } from "./store";
-import { GnoWSProvider } from "@gnolang/gno-js-client";
+import { GnoJSONRPCProvider } from "@gnolang/gno-js-client";
 import { useEffect } from "react";
 import { constants } from "./constants";
 import Home from "./pages";
@@ -8,7 +8,7 @@ function App() {
   const { setProvider } = useProviderStore();
 
   useEffect(() => {
-    const provider = new GnoWSProvider(constants.chainRPC);
+    const provider = new GnoJSONRPCProvider(constants.chainRPC);
     setProvider(provider);
   }, [setProvider]);
 
